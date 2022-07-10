32ff7d09685c5f6e7ae1f7fdc3e1cb1af41a2fdc
wrfw <wrfw@Voldermot.com>
2022-07-10 17:31:04 +0200

    Add function that calculate, so basicaly calculator's working. Time to add some beauty for this ugly beast

--- calcium/src/screens/SimpleCalc/SimpleCalc.js
+++ calcium/src/screens/SimpleCalc/SimpleCalc.js
@@ -12,13 +12,94 @@ import Button from '../../components/Button'
 
 const SimpleCalc = () => {
   const [number, setNumber] = useState("0")
-
+  const [operations, setOperations] = useState("+-X:%")
   function appendSymbol(newSymbol){
     if(number == "0"){
       setNumber(newSymbol)
     }else{
       setNumber(number+newSymbol)
     }
+  }
+
+  function compute(){
+    let firstNumber = ""
+    let secondNumber = ""
+    let operation = ""
+    let state = 0
+    for(let i = 0; i<number.length; i++){
+      if(operations.includes(number[i])){
+        if(secondNumber.length!=0){
+          console.log(firstNumber)
+          console.log(secondNumber)
+          switch (operation) {
+            case "+":
+              firstNumber = ""+(parseFloat(firstNumber)+parseFloat(secondNumber))
+              secondNumber = ""
+              break;
+            case "-":
+              firstNumber = ""+(parseFloat(firstNumber)-parseFloat(secondNumber))
+              secondNumber = ""
+              break;
+            case "X":
+              firstNumber = ""+(parseFloat(firstNumber)*parseFloat(secondNumber))
+              secondNumber = ""
+              break;
+            case ":":
+              if(secondNumber=="0"){
+                setNumber("NaN");
+                break;
+              }
+              firstNumber = ""+(parseFloat(firstNumber)/parseFloat(secondNumber))
+              secondNumber = ""
+              break;
+            case "%":
+              firstNumber = ""+(parseFloat(firstNumber)-parseFloat(secondNumber))
+              secondNumber = ""
+              break;
+            default:
+          operation=number[i]
+          }
+        }else{
+          operation=number[i]
+        }
+      }else{
+        if(operation.length==0){
+          firstNumber=firstNumber+number[i]
+        }else{
+          secondNumber=secondNumber+number[i]
+        }
+      }
+    }
+    if(secondNumber.length!=0){
+      switch (operation) {
+        case "+":
+          firstNumber = ""+(parseFloat(firstNumber)+parseFloat(secondNumber))
+          secondNumber = ""
+          break;
+        case "-":
+          firstNumber = ""+(parseFloat(firstNumber)-parseFloat(secondNumber))
+          secondNumber = ""
+          break;
+        case "X":
+          firstNumber = ""+(parseFloat(firstNumber)*parseFloat(secondNumber))
+          secondNumber = ""
+          break;
+        case ":":
+          if(secondNumber=="0"){
+            setNumber("NaN");
+            break;
+          }
+          firstNumber = ""+(parseFloat(firstNumber)/parseFloat(secondNumber))
+          secondNumber = ""
+          break;
+        case "%":
+          firstNumber = ""+(parseFloat(firstNumber)-parseFloat(secondNumber))
+          secondNumber = ""
+          break;
+        default:
+      }
+    }
+    setNumber(firstNumber)
   }
 
   return (
@@ -50,132 +131,125 @@ const SimpleCalc = () => {
         </View>
         <View style={styles.smallButton}>
           <Button
-            text="X"
+            text="x"
             fgc="white"
             bgc="grey"
-            onPress={()=>{console.log("xxd")}} />
+            onPress={() => appendSymbol("X")} />
         </View>
-     </View>
-     <View style={styles.buttonArea}>
-       <View style={styles.smallButton}>
-         <Button
-           text="7"
-           fgc="white"
-           bgc="grey"
-           onPress={() => appendSymbol("7")} />
-       </View>
-       <View style={styles.smallButton}>
-         <Button
-           text="8"
-           fgc="white"
-           bgc="grey"
-           onPress={()=>{console.log("xxd")}} />
-       </View>
-       <View style={styles.smallButton}>
-         <Button
-           text="9"
-           fgc="white"
-           bgc="grey"
-           onPress={()=>{console.log("xxd")}} />
-       </View>
-       <View style={styles.smallButton}>
-         <Button
-           text="-"
-           fgc="white"
-           bgc="grey"
-           onPress={()=>{console.log("xxd")}} />
-       </View>
-    </View>
-    <View style={styles.buttonArea}>
-      <View style={styles.smallButton}>
-        <Button
-          text="4"
-          fgc="white"
-          bgc="grey"
-          onPress={()=>{console.log("xd")}} />
       </View>
-      <View style={styles.smallButton}>
-        <Button
-          text="5"
-          fgc="white"
-          bgc="grey"
-          onPress={()=>{console.log("xxd")}} />
+      <View style={styles.buttonArea}>
+        <View style={styles.smallButton}>
+          <Button
+            text="7"
+            fgc="white"
+            bgc="grey"
+            onPress={() => appendSymbol("7")} />
+        </View>
+        <View style={styles.smallButton}>
+          <Button
+            text="8"
+            fgc="white"
+            bgc="grey"
+            onPress={() => appendSymbol("8")} />
+        </View>
+        <View style={styles.smallButton}>
+          <Button
+            text="9"
+            fgc="white"
+            bgc="grey"
+            onPress={() => appendSymbol("9")} />
+        </View>
+        <View style={styles.smallButton}>
+          <Button
+            text="-"
+            fgc="white"
+            bgc="grey"
+            onPress={() => appendSymbol("-")} />
+        </View>
       </View>
-      <View style={styles.smallButton}>
-        <Button
-          text="6"
-          fgc="white"
-          bgc="grey"
-          onPress={()=>{console.log("xxd")}} />
+      <View style={styles.buttonArea}>
+        <View style={styles.smallButton}>
+          <Button
+            text="4"
+            fgc="white"
+            bgc="grey"
+            onPress={() => appendSymbol("4")} />
+        </View>
+        <View style={styles.smallButton}>
+          <Button
+            text="5"
+            fgc="white"
+            bgc="grey"
+            onPress={() => appendSymbol("5")} />
+        </View>
+        <View style={styles.smallButton}>
+          <Button
+            text="6"
+            fgc="white"
+            bgc="grey"
+            onPress={() => appendSymbol("6")} />
+        </View>
+        <View style={styles.smallButton}>
+          <Button
+            text="+"
+            fgc="white"
+            bgc="grey"
+            onPress={() => appendSymbol("+")} />
+        </View>
       </View>
-      <View style={styles.smallButton}>
-        <Button
-          text="+"
-          fgc="white"
-          bgc="grey"
-          onPress={()=>{console.log("xxd")}} />
+      <View style={styles.buttonArea}>
+        <View style={styles.smallButton}>
+          <Button
+            text="1"
+            fgc="white"
+            bgc="grey"
+            onPress={() => appendSymbol("1")} />
+        </View>
+        <View style={styles.smallButton}>
+          <Button
+            text="2"
+            fgc="white"
+            bgc="grey"
+            onPress={() => appendSymbol("2")} />
+        </View>
+        <View style={styles.smallButton}>
+          <Button
+            text="3"
+            fgc="white"
+            bgc="grey"
+            onPress={() => appendSymbol("3")} />
+        </View>
+        <View style={styles.smallButton}>
+          <Button
+            text="/"
+            fgc="white"
+            bgc="grey"
+            onPress={() => appendSymbol(":")} />
+        </View>
       </View>
-   </View>
-   <View style={styles.buttonArea}>
-     <View style={styles.smallButton}>
-       <Button
-         text="1"
-         fgc="white"
-         bgc="grey"
-         onPress={()=>{console.log("xd")}} />
-     </View>
-     <View style={styles.smallButton}>
-       <Button
-         text="2"
-         fgc="white"
-         bgc="grey"
-         onPress={()=>{console.log("xxd")}} />
-     </View>
-     <View style={styles.smallButton}>
-       <Button
-         text="3"
-         fgc="white"
-         bgc="grey"
-         onPress={()=>{console.log("xxd")}} />
-     </View>
-     <View style={styles.smallButton}>
-       <Button
-         text="="
-         fgc="white"
-         bgc="grey"
-         onPress={()=>{console.log("xxd")}} />
-     </View>
-  </View>
-  <View style={styles.buttonArea}>
-    <View style={styles.smallButton}>
-      <Button
-        text="0"
-        fgc="white"
-        bgc="grey"
-        onPress={()=>{console.log("xd")}} />
-    </View>
-    <View style={styles.smallButton}>
-      <Button
-        text="0"
-        fgc="white"
-        bgc="grey"
-        onPress={()=>{console.log("xxd")}} />
-    </View>
-    <View style={styles.smallButton}>
-      <Button
-        text="."
-        fgc="white"
-        bgc="grey"
-        onPress={()=>{console.log("xxd")}} />
-    </View>
-    <View style={styles.smallButton}>
-      <Button
-        text=":"
-        fgc="white"
-        bgc="grey"
-        onPress={()=>{console.log("xxd")}} />
-    </View>
- </View>
+      <View style={styles.buttonArea}>
+        <View style={styles.zeroButton}>
+          <Button
+            text="0"
+            fgc="white"
+            bgc="grey"
+            onPress={() => appendSymbol("0")} />
+        </View>
+        <View style={styles.smallButton}>
+          <Button
+            text="."
+            fgc="white"
+            bgc="grey"
+            onPress={() => appendSymbol(".")} />
+        </View>
+        <View style={styles.smallButton}>
+          <Button
+            text="="
+            fgc="white"
+            bgc="grey"
+            onPress={() => compute()} />
+        </View>
+      </View>
     </View>
   );
 };
@@ -191,6 +265,11 @@ const styles = StyleSheet.create({
     borderWidth:5,
     borderColor:"white",
     flex:1
+  },
+  zeroButton:{
+    borderWidth:5,
+    borderColor:"white",
+    flex:2
   },
   buttonArea:{
     flexWrap:"wrap",
